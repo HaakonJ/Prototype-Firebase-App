bd40507b6c685b9bc2a0b7f5a043c06bf2d96f59
HaakonJ <haakon@haakonjakobson.com>
2017-08-19 20:53:09 -0700

    update to firebase init script

--- js/map.js
+++ js/map.js
@@ -1,12 +1,8 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",
-    authDomain: "test-project-4d8c5.firebaseapp.com",
-    databaseURL: "https://test-project-4d8c5.firebaseio.com",
-    projectId: "test-project-4d8c5",
-    storageBucket: "test-project-4d8c5.appspot.com",
-    messagingSenderId: "591336136232"
-};
+src = "https://www.gstatic.com/firebasejs/4.3.0/firebase.js"
+
+  // Initialize Firebase
+ 
+var config = {   apiKey: "AIzaSyC42UIRETC_VVO7cXGqo9ru8TlfDi_-El8",   authDomain: "test-project-4d8c5.firebaseapp.com",   databaseURL: "https://test-project-4d8c5.firebaseio.com",   projectId: "test-project-4d8c5",   storageBucket: "test-project-4d8c5.appspot.com",   messagingSenderId: "591336136232"  }; 
 firebase.initializeApp(config);
 
 //Get elements
